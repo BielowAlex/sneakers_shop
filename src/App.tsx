2e75b2c0385d289e322eba66cfdaf4ac9e0026ae
BielowAlex <bielow.alex@gmail.com>
2022-08-23 14:30:27 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
 import * as React from "react";
-import {Routes, Route} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 
 import './styles/style.css'
-import {Cart, Favorite, Home} from "./Pages";
 import {MainLayout} from "./Layouts/MainLayout";
-
-
+import { Favorite, Home} from "./Pages";
 
 
 const App: React.FC = () => {
     const [overlayShow, setOverlayShow] = React.useState(false);
-    console.log(localStorage.favoriteList)
-
     const setOverlay = () => {
         setOverlayShow(!overlayShow);
     }
-
     return (
         <Routes>
-            <Route path="/" element={<MainLayout setOverlay={setOverlay}/>}>
-                <Route index element={<Home setOverlay={setOverlay} overlayShow={overlayShow}/>}/>
-                <Route path="/cart" element={<Cart/>}/>
-                <Route path="/favorite" element={<Favorite setOverlay={setOverlay} overlayShow={overlayShow}/>}/>
+            <Route path="/" element={<MainLayout setOverlay={setOverlay} overlayShow={overlayShow}/>}>
+                <Route index element={<Home/>}/>
+                <Route path="/favorite" element={<Favorite/>}/>
             </Route>
         </Routes>
     );
