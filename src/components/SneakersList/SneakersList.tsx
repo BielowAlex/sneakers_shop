64f3885f171dd813d55483e196a0b1c9227ebefb
bielowalex <drohomyretskyi.oleksandr@ukr.net>
2023-01-24 03:25:50 +0200

    update class names in SneakersList

--- src/components/SneakersList/SneakersList.tsx
+++ src/components/SneakersList/SneakersList.tsx
@@ -13,13 +13,13 @@ const SneakersList: React.FC = () => {
     }, [dispatch]);
 
     return (
-        <div className={sneakersList.length > 0 ? 'sneakers_list' : 'empty_sneakers_list'}>
+        <div className={sneakersList.length > 0 ? 'sneakers-list' : 'sneakers-list--empty'}>
             {sneakersList.length > 0
                 ? sneakersList.map(sneakers => <SneakersCard sneakers={sneakers} key={sneakers.id}/>)
-                : <div className="cart_empty">
-                    <img height={100} width={100} src="/img/sad.png" alt="empty box"/>
-                    <h2>List is empty</h2>
-                    <p>Such beauties are not in our shop</p>
+                : <div className="sneakers-list--empty__ad">
+                    <img className="sneakers-list--empty__ad-img" height={100} width={100} src="/img/sad.png" alt="empty box"/>
+                    <h2 className="sneakers-list--empty__ad-title">List is empty</h2>
+                    <p className="sneakers-list--empty__ad-advice">Such beauties are not in our shop</p>
                 </div>
             }
         </div>
